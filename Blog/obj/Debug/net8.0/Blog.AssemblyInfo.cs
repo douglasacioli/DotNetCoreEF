d77f147805cc5cc1483a2cfb9b0f34d95b12bea1
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2024-06-25 19:46:12 -0300

    lista estatica de exibição

--- Blog/obj/Debug/net8.0/Blog.AssemblyInfo.cs
+++ Blog/obj/Debug/net8.0/Blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be505622b023f22823b5df155c41fc058b781d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
