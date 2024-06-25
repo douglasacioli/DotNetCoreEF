5bddf314a639a288eca769e668b99f752c5423a9
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2024-06-25 20:06:46 -0300

    criando uma lista do nosso modelo e transportando para uma view utilizando ViewBag

---
+++ Blog/Controllers/PostController.cs
@@ -0,0 +1,21 @@
+using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class PostController : Controller
+    {
+         public IActionResult Index()
+        {
+            var listaDePosts = new List<Post>()
+            {
+                new Post() {Titulo = "Harry Potter 1", Resumo="Pedra Filosofal", Categoria="Filme, Livro"},
+                new Post() {Titulo = "Cassino Royale", Resumo="007", Categoria="Filme"},
+                new Post() {Titulo = "Monge e o Executivo", Resumo="Romance sobre liderança", Categoria="livro"},
+                new Post() {Titulo = "New York, New York", Resumo="Sucesso de Frank Sinatra", Categoria="Música"},
+            };
+            ViewBag.Posts = listaDePosts;
+            return View();
+        }
+    }
+}
